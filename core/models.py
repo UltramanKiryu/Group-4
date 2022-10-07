c7048df6c89e2da4c8c6843a82675b5d964b959b
Andrew Banks <90979707+UltramanKiryu@users.noreply.github.com>
2022-10-07 15:57:45 -0500

    Add files via upload

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank_profile_picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def _str_(self):
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField(max_length=50)
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.user

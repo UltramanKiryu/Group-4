cb3de84665274e7edb900a1695ec23143cb95927
Logan Christopher <logan.christopher@comcast.net>
2022-10-10 17:26:42 -0500

    - added comment for future friend post integration

--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
-    posts = Post.objects.filter(user=user_object.username)
+    posts = Post.objects.filter(user=user_object.username) # add friend list filter
     return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 @login_required(login_url='signin')

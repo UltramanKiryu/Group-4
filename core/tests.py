7961ffe9fcfc342b278f3c6e5e08466b284c8dbd
Logan Christopher <logan.christopher@comcast.net>
2022-11-03 18:38:46 -0500

    - finished login cases

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.contrib.sessions.middleware import SessionMiddleware
-from django.test import RequestFactory, TestCase
+from django.contrib.messages.middleware import MessageMiddleware
+from django.test import RequestFactory, TestCase, Client
+from django.contrib import messages
+
 
 from core.models import Profile
 from core.views import signin
@@ -8,40 +11,65 @@ from core.views import signin
 # https://docs.djangoproject.com/en/4.1/intro/tutorial05/
 class TestCases(TestCase):
     def setUp(self):
-        print("\n[*] Running setup...")
-        # every test needs access to RequestFactory to build requests
+        # every test needs access to RequestFactory to build http requests
         # https://docs.djangoproject.com/en/4.1/topics/testing/advanced/#django.test.RequestFactory
         self.factory = RequestFactory()
 
-        # create a django user and social book profile obj in db
+        # create a django user and social book profile obj in temporary db
         self.user = User.objects.create_user(username='logan', email='logan@email.com', password='123')
         self.profile = Profile.objects.create(user=self.user, id_user=self.user.id)
-        self.profile.bio = "tests are the best"
-        self.profile.location = "earth"
+        self.profile.bio = 'tests are the best'
+        self.profile.location = 'earth'
 
-        print("[*] Created new User\n  username: "+self.user.username+"\n  email: "+self.user.email+"\n  password: 123")
-        print("[*] Created new Profile\n  id: "+str(self.user.id)+"\n  bio: "+self.profile.bio+"\n  location: "+self.profile.location)
-
-    def test_signin_success(self):
-        print("\n[*] Testing signin success...")
-
-        # craft a post request to signin as the user created in setup
-        request = self.factory.post('/signin', {'username': 'logan', 'password': '123'})
-
-        # add session middleware manually
+    # add default middleware config to request
+    def AddMiddleware(self, request):
+        # add middleware manually
         middleware = SessionMiddleware(request)
         middleware.process_request(request)
-
-        # simulate a logged-in user by setting request.user manually.
+        request._messages = messages.storage.default_storage(request)
         request.user = self.user
 
-        # test view as if it were deployed at /signin
-        print("[*] Request: %s" % request)
-        print("[*] Attempting sign in...")
+    # return the first message in storage as a string
+    def get_message(self, request):
+        storage = messages.get_messages(request)
+        message = None
+        for message in storage:
+            break
+        return str(message)
+
+    def test_signin_success(self):
+        # craft a post request to signin as the user created in setup
+        request = self.factory.post('/signin', {'username': self.user.username, 'password': '123'})
+        self.AddMiddleware(request)
+
         response = signin(request)
-        print("[*] Response: %s" % response)
+        response.client = Client()
         
-        # redirect to / on success
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, "/")
+        # 302 redirect to / on success
+        self.assertRedirects(response, '/', target_status_code=302)
         
+    def test_signin_failure_username(self):
+        # craft a post request to signin with the wrong username
+        request = self.factory.post('/signin', {'username': 'logan2', 'password': '123'})
+        self.AddMiddleware(request)
+
+        response = signin(request)
+        response.client = Client()
+        message = self.get_message(request)
+
+        # respond with message and redirect to /signin on failure
+        self.assertEqual(message, 'Credentials Invaild')
+        self.assertRedirects(response, '/signin')
+
+    def test_signin_failure_password(self):
+        # craft a post request to signin with the wrong username
+        request = self.factory.post('/signin', {'username': self.user.username, 'password': '124'})
+        self.AddMiddleware(request)
+
+        response = signin(request)
+        response.client = Client()
+        message = self.get_message(request)
+
+        # respond with message and redirect to /signin on failure
+        self.assertEqual(message, 'Credentials Invaild')
+        self.assertRedirects(response, '/signin')

c76108631a99d61e30a2ffb6a8cb5723b6e5834c
Andrew Banks <90979707+UltramanKiryu@users.noreply.github.com>
2022-11-04 17:07:46 -0500

    Update views.py

--- core/views.py
+++ core/views.py
@@ -235,24 +235,6 @@ def comment_post(request):
         return redirect('/')
 
 @login_required(login_url='signin')
-def follow(request):
-    if request.method == 'POST':
-        follower = request.POST['follower']
-        user = request.POST['user']
-
-        if FollowersCount.objects.filter(follower=follower, user=user).first():
-            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
-            delete_follower.delete()
-            return redirect('/profile/'+user)
-        else:
-            new_follower = FollowersCount.objects.create(follower=follower, user=user)
-            new_follower.save()
-            return redirect('/profile/'+user)
-
-    else:
-        return redirect('/')
-
-@login_required(login_url='signin')
 def delete(request):
     username = request.user.username
     post_id = request.GET.get('post_id')

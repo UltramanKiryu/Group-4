eb2cef9d46023b0deef8bbaee817faf3dc6191ca
Logan Christopher <logan.christopher@comcast.net>
2022-11-03 19:00:18 -0500

    - quick refactor

--- core/tests.py
+++ core/tests.py
@@ -5,7 +5,7 @@ from django.test import RequestFactory, TestCase, Client
 from django.contrib import messages
 
 from core.models import Profile
-from core.views import signin
+from core.views import signin, logout
 
 # https://docs.djangoproject.com/en/4.1/intro/tutorial05/
 class TestCases(TestCase):
@@ -23,7 +23,7 @@ class TestCases(TestCase):
     # request to signin as the user created in setup
     def test_signin_success(self):
         request = self.factory.post('/signin', {'username': self.user.username, 'password': '123'})
-        AddMiddleware(self, request)
+        AddMiddleware(request)
 
         response = signin(request)
         response.client = Client()
@@ -34,7 +34,7 @@ class TestCases(TestCase):
     # request to signin with the wrong username
     def test_signin_failure_username(self):
         request = self.factory.post('/signin', {'username': 'logan2', 'password': '123'})
-        AddMiddleware(self, request)
+        AddMiddleware(request)
 
         response = signin(request)
         response.client = Client()
@@ -47,7 +47,7 @@ class TestCases(TestCase):
     # request to signin with the wrong password
     def test_signin_failure_password(self):
         request = self.factory.post('/signin', {'username': self.user.username, 'password': '124'})
-        AddMiddleware(self, request)
+        AddMiddleware(request)
 
         response = signin(request)
         response.client = Client()
@@ -57,15 +57,26 @@ class TestCases(TestCase):
         self.assertEqual(message, 'Credentials Invaild')
         self.assertRedirects(response, '/signin')
 
+    # request to logout the user created at setup
+    def test_logout(self):
+        request = self.factory.get('/logout')
+        AddMiddleware(request)
+
+        # simulate authenticated user
+        request.user = self.user
+
+        response = logout(request)
+        response.client = Client()
+
+        self.assertRedirects(response, '/signin')
+
 # Helpers ---------------------------------------------------------------
 
 # add default middleware config to request
-def AddMiddleware(self, request):
-    # add middleware manually
+def AddMiddleware(request):
     middleware = SessionMiddleware(request)
     middleware.process_request(request)
     request._messages = messages.storage.default_storage(request)
-    request.user = self.user
 
 # return the first message in storage or None if empty
 def get_message(request):

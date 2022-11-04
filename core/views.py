97a4f110419390e6993f45b45657fb2928c9c255
Andrew Banks <90979707+UltramanKiryu@users.noreply.github.com>
2022-11-04 17:05:43 -0500

    Update views.py

--- core/views.py
+++ core/views.py
@@ -235,43 +235,6 @@ def comment_post(request):
         return redirect('/')
 
 @login_required(login_url='signin')
-def profile(request, pk):
-    user_object = User.objects.get(username=pk)
-    user_profile = Profile.objects.get(user=user_object)
-    user_posts = Post.objects.filter(user=pk)
-    user_post_length = len(user_posts)
-
-    comments = []
-
-    for posts in user_posts:
-        comment_list = Comment.objects.filter(post_id=posts.id)
-        comments.append(comment_list)
-
-    post_comments = list(chain(*comments))
-
-    follower = request.user.username
-    user = pk
-
-    if FollowersCount.objects.filter(follower=follower, user=user).first():
-        button_text = 'Unfollow'
-    else:
-        button_text = 'Follow'
-
-    user_followers = len(FollowersCount.objects.filter(user=pk))
-    user_following = len(FollowersCount.objects.filter(follower=pk))
-    context = {
-        'user_object': user_object,
-        'user_profile': user_profile,
-        'user_posts': user_posts,
-        'user_post_length': user_post_length,
-        'button_text': button_text,
-        'user_followers': user_followers,
-        'user_following': user_following,
-        'post_comments': post_comments,
-    }
-    return render(request,'profile.html',context)
-
-@login_required(login_url='signin')
 def follow(request):
     if request.method == 'POST':
         follower = request.POST['follower']
